07e2a43fb2579a5f8f4c7cb121d1c97ae79045d8
John <johnnyqm5@gmail.com>
2022-01-21 17:18:11 -0600

    NavBar,Footer and Layout
    Creation of NavBar,footer and Layout

---
+++ components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "/styles/NavBar.module.css";
+const NavBar = () => {
+	return <div className={styles.container}>NavBar</div>;
+};
+
+export default NavBar;
