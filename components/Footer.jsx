72df690b1bc2db5c3beb1b9bbc49a9b1d9a28408
John <58703369+johnsistem@users.noreply.github.com>
2022-01-23 23:28:33 -0600

    navbar and Featured
    navbar and Featured

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { styles } from "/styles/NavBar.module.css";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div
+			style={{
+				backgroundColor: "#ff22",
+				display: "flex",
+				justifyContent: "space-between",
+			}}
+		>
+			<div style={{ backgroundColor: "#3b3b3021" }}>rojo555</div>
+			<div style={{ backgroundColor: "#3b3b3021" }}>centro54</div>
+			<div style={{ backgroundColor: "#3b3b3021" }}>right</div>
+		</div>
+	);
 };
 
 export default Footer;
