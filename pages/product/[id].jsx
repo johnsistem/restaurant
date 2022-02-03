9e88dfc99ee8eb11fc28c968295af2001bb88ec4
John <58703369+johnsistem@users.noreply.github.com>
2022-02-02 22:23:09 -0600

    created components,footer and images

---
+++ pages/product/[id].jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import styles from "../../styles/product.module.css";
+import Image from "next/image";
+
+const Product = () => {
+	const [size, setSize] = useState(0);
+	const pizza = {
+		id: 1,
+		img: "/img/pizza.png",
+		name: "CAMPAGNOLA",
+		price: [19.9, 23.9, 27.9],
+		desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.",
+	};
+	return (
+		<div className={styles.container}>
+			<div className={styles.left}>
+				<div className={styles.imgContainer}>
+					<Image
+						src={pizza.img}
+						objectFit="contain"
+						alt=""
+						layout="fill"
+					/>
+				</div>
+			</div>
+			<div className={styles.right}>
+				<h1 className={styles.title}>{pizza.name}</h1>
+				<span className={styles.price}>${pizza.price[size]}</span>
+				<p className={styles.desc}>{pizza.desc}</p>
+				<h3 className={styles.choose}>Choose the size</h3>
+				<div className={styles.sizes}>
+					<div className={styles.size}>
+						<Image src="/img/size.png" layout="fill" alt=""/>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Product;
